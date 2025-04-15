18db8206c8f35edeb4ff33948a18283527957aaa
Amama Rida <74170920+ridamaqdoom@users.noreply.github.com>
2025-04-15 13:54:15 -0600

    Added the random_forest.py file

--- analyser/random_forest.py
+++ analyser/random_forest.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+
 # === Optional: Hide dtype warnings ===
 warnings.filterwarnings('ignore', category=pd.errors.DtypeWarning)
 
