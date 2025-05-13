cdfcbc3d91fea8bf9ebd053e14257cd1c6a81eab
maryam <msajjad@vendasta.com>
2025-05-12 22:37:00 -0600

    clean up unused code

--- analyser/main_analyser.py
+++
@@ -1,8 +0,0 @@
-import ibis
-
-calgary_occupancy = open("../dataset/calgary-occupancy.csv", "r")
-toronto_occupancy = open("../dataset/toronto-occupancy-2025.csv", "r")
-# print(calgary_occupancy.read())
-print(toronto_occupancy.read())
-
-
